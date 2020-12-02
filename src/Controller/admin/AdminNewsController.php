cea19a21734bc6deb31410640c74cabfe3498db0
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-12-02 17:07:39 +0100

    Création Page New twig et de AdminNewsController

--- src/Controller/admin/AdminNewsController.php
+++ src/Controller/admin/AdminNewsController.php
@@ -4,7 +4,17 @@
 namespace App\Controller\admin;
 
 
-class AdminNewsController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+class AdminNewsController extends AbstractController
+{
+    /**
+     * @Route("/admin/news", name="admin_news_list")
+     */
+
+    public function newsList()
+    {
+        return $this->render('admin/news.html.twig');
+    }
 }
