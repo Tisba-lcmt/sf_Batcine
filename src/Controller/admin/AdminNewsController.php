85464d056a770dfffaa82b3786f73118561e5cca
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-12-03 16:39:20 +0100

    Entity News, insert, update, delete twig, prépa CRUD

--- src/Controller/admin/AdminNewsController.php
+++ src/Controller/admin/AdminNewsController.php
@@ -17,4 +17,32 @@ class AdminNewsController extends AbstractController
     {
         return $this->render('admin/news.html.twig');
     }
+
+    /**
+     * @Route("/admin/new/insert", name="admin_new_insert")
+     */
+
+    public function insertNew()
+    {
+        return $this->render('admin/insertNews.html.twig');
+    }
+
+    /**
+     * @Route("/admin/new/update", name="admin_new_update")
+     */
+
+    public function updateNew()
+    {
+        return $this->render('/admin/updateNews.html.twig');
+    }
+
+    /**
+     * @Route("/admin/new/delete", name="admin_new_delete")
+     */
+
+    public function deleteNew()
+    {
+        return $this->render('/admin/deleteNews.html.twig');
+    }
+
 }
