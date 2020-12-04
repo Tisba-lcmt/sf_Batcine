6d5e6ac3ae739ed72e81cbd94fb0c4717a372f4b
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-12-05 00:06:43 +0100

    Création de NewsType, base Admin Twig, button CRUD News Twig et CSS

---
+++ src/Form/NewsType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\News;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('image')
+            ->add('creationDate', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('publicationDate', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('isPublished')
+            ->add('Envoyer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => News::class,
+        ]);
+    }
+}
