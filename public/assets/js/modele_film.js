b980fd113211764bd27ba7803748a25f50892106
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-12-07 23:03:10 +0100

    MAJ Responsive Mobile, base, home, news, movies, currentMovies, upcomingMovies, series, movie Twig, CSS & JS

--- public/assets/js/modele_film.js
+++ public/assets/js/modele_film.js
@@ -1,5 +1,13 @@
-let stars = document.querySelectorAll('.groupe_icon img');
-console.log(stars)
-for(let i = 0; i < stars.length; i++){
-    
-}
+// let stars = document.querySelectorAll('.groupe_icon img');
+// console.log(stars)
+// for(let i = 0; i < stars.length; i++){
+
+// }
+
+
+let plus = document.querySelector('.plus')
+let slider = document.querySelector('.slider');
+
+plus.addEventListener('click',function(e){
+    slider.classList.toggle('show');
+})
