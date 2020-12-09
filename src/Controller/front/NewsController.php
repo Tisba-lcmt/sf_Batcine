2d7b4ff29a24c8b22bb6ff0f4eb95f8a8e7f158a
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-12-09 19:16:00 +0100

    methode newsList findBy par Ordre DECS de publicationDate

--- src/Controller/front/NewsController.php
+++ src/Controller/front/NewsController.php
@@ -6,17 +6,20 @@ namespace App\Controller\front;
 
 use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NewsController extends AbstractController
 {
     /**
      * @Route("/news", name="news_list")
+     * @param NewsRepository $newsRepository
+     * @return Response
      */
 
     public function newsList(NewsRepository $newsRepository)
     {
-        $news = $newsRepository->findAll();
+        $news = $newsRepository->findBy([],['publicationDate' => 'DESC']);
 
         return $this->render('front/news.html.twig', [
             'news' => $news
