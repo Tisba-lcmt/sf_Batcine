b982d44c56f91ebc7a643092a431e2ccb2eea0be
Lucmaret <baptistelucmaret@wanadoo.fr>
2020-12-10 16:09:48 +0100

    Maj News twig Indentation

--- src/Controller/front/PagesController.php
+++ src/Controller/front/PagesController.php
@@ -9,7 +9,6 @@ use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\DateTime;
 
 class PagesController extends AbstractController
 {
@@ -23,27 +22,33 @@ class PagesController extends AbstractController
 
     public function home(NewsRepository $newsRepository, MediaRepository $mediaRepository)
     {
-        //Je récupere mes deux dernieres actus
+
+        // J'effectue une requête select en bdd pour récupérer mes 2 actualités
+        // les plus récentes (dernières actus) et je stock le résultat dans la variable $lastNews.
+
+        // Cette méthode limit permet de ne pas récupérer toutes mes actu pour en prendre que 2.
         $lastNews = $newsRepository->findBy([], ['publicationDate' => 'DESC'], 2, null);
 
-        //je définie la date d'aujourdhui
+        // Je stock dans une variable $now, la date d'aujourd'hui à l'aide de la classe DateTime
         $now = new \DateTime('now');
 
-        //je recupe mes films ( les plus récents )
+        // Je récupére mes films les plus récents en fonction de la date de Sortie
         $movies = $mediaRepository->findBy([], ['releaseDate' => 'DESC'], null, null);
 
-        //je déclare mes array
+        // Je déclare mes différents array pour pouvoir stocker mes résultats
         $currentMovies = [];
-
         $upComingMovies = [];
 
-        //je trie a chaque boucle le film par rapport a la date d'ajourdhui
+        // Je trie à chaque tour de boucle le film par rapport a la date d'ajourd'hui
         foreach ($movies as $movie)
         {
+            // Si ma date de Sortie est inférieure à la date d'aujourd'hui
             if($movie->getReleaseDate() < $now)
             {
+                // Je la stocke dans la variable film à l'affiche
                $currentMovies[] = $movie;
             }else{
+                // Je la stocke dans la variable film prochainement
                 $upComingMovies[] = $movie;
             }
         }
